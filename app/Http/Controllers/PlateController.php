0a95e06645d3a5571d24e3013c44ac477679a1da
Matheus Manzoli <manzoli.contato@gmail.com>
2022-08-10 16:47:17 -0300

    Update PlateController.php

--- app/Http/Controllers/PlateController.php
+++ app/Http/Controllers/PlateController.php
@@ -30,37 +30,39 @@ class PlateController extends Controller
         $tipos_veiculo = ["Automovel", "Motocicleta"];
 
         $data = [
-            "veiculo" => [
-                "uf" => "MS",
-                "ano" => "2013/2013",
-                "cmt" => "335",
-                "cor" => "Prata",
-                "pbt" => "185",
-                "placa" => $plate,
-                "chassi" => "CHASSI",
-                "n_motor" => "MOTOR",
-                "potencia" => "142",
-                "municipio" => "CIDADE",
-                "carroceria" => null,
-                "cilindradas" => "1998",
-                "combustivel" => "Alcool / Gasolina",
-                "procedencia" => "Nacional",
-                "caixa_cambio" => null,
-                "marca_modelo" => "Bmw\/x1 S201 Active Flex",
-                "tipo_montagem" => "Completa",
-                "tipo_carroceria" => null,
-                "tipo_de_veiculo" => $tipos_veiculo[array_rand($tipos_veiculo)],
-                "eixo_traseiro_dif" => null,
-                "situacao_do_chassi" => null,
-                "capacidade_de_carga" => "49",
-                "quantidade_de_eixos" => "2",
-                "quantidade_passageiro" => "5"
-            ],
-            "fipes" => [
-                [
-                    "valor" => rand(20000, 200000),
-                    "codigo" => "025186-0",
-                    "marca_modelo" => "Renault Duster Dynamique 4x4 2.0 Hi-Flex 16v Mec"
+            "data" =>[
+                "veiculo" => [
+                    "uf" => "MS",
+                    "ano" => "2013/2013",
+                    "cmt" => "335",
+                    "cor" => "Prata",
+                    "pbt" => "185",
+                    "placa" => $plate,
+                    "chassi" => "CHASSI",
+                    "n_motor" => "MOTOR",
+                    "potencia" => "142",
+                    "municipio" => "CIDADE",
+                    "carroceria" => null,
+                    "cilindradas" => "1998",
+                    "combustivel" => "Alcool / Gasolina",
+                    "procedencia" => "Nacional",
+                    "caixa_cambio" => null,
+                    "marca_modelo" => "Bmw\/x1 S201 Active Flex",
+                    "tipo_montagem" => "Completa",
+                    "tipo_carroceria" => null,
+                    "tipo_de_veiculo" => $tipos_veiculo[array_rand($tipos_veiculo)],
+                    "eixo_traseiro_dif" => null,
+                    "situacao_do_chassi" => null,
+                    "capacidade_de_carga" => "49",
+                    "quantidade_de_eixos" => "2",
+                    "quantidade_passageiro" => "5"
+                ],
+                "fipes" => [
+                    [
+                        "valor" => rand(20000, 200000),
+                        "codigo" => "025186-0",
+                        "marca_modelo" => "Renault Duster Dynamique 4x4 2.0 Hi-Flex 16v Mec"
+                    ]
                 ]
             ]
         ];
